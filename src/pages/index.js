e784bf1d4a91c1e5a6ca32d0659ae35de7d5878c
= <anguyen031@gmail.com>
2022-04-26 16:49:16 -0700

    Working build

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 
 export default function Home() {
-	return <StaticImage src="../images/transparent-koi.png" alt="10 ceramic koi" />;
+	return (
+		<StaticImage
+			src="../images/transparent-koi.png"
+			alt="10 ceramic koi"
+			placeholder="blurred"
+			layout="fixed"
+			width={200}
+		/>
+	);
 }
